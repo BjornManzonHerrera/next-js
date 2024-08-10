68d9d7f9ce7e965affa7bfcf1759ef4533b1069b
Bjorn Manzon Herrera <bjornmanzonherrera@gmail.com>
2024-08-10 10:53:30 +0800

    initial commit

---
+++ src/app/Education/page.js
@@ -0,0 +1,18 @@
+import styles from "./page.module.css";
+import Link from "next/link";
+
+export default function Home() {
+    return(
+        <main className="styles.main">
+            <h1 className="styles.main">Hello</h1>
+            <h3></h3>
+
+            <div>
+                <link></link>
+            </div>
+        </main>
+    )
+
+
+
+}
